a8ff9f108fac3c02818423a6a6b0f6db1c164a43
Junior Oliveira <arojunior@gmail.com>
2018-03-19 13:27:06 -0300

    add support to parse Swagger format

---
+++ src/swagger/parseSwaggerDocumentation.js
@@ -0,0 +1,15 @@
+import Api from '../Api'
+import handleJson, { removeTrailingSlash } from './handleJson'
+
+export default function parseSwaggerDocumentation(entrypointUrl) {
+  entrypointUrl = removeTrailingSlash(entrypointUrl)
+  return fetch(entrypointUrl).then(res => res.json()).then(response => {
+      
+      const title = response.info.title;
+      const resources = handleJson(response, entrypointUrl);
+            
+      return Promise.resolve({ api: new Api(entrypointUrl, {title, resources}), response , status: response.status });
+    },
+    ({ response }) => Promise.reject({ api: new Api(entrypointUrl, {resources: []}), response, status: response.status })
+  );
+}
