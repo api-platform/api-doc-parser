d6ba655a233452f2e6ac00b1976e140b8ad7c4e1
Simon Hildebrandt <simonhildebrandt@gmail.com>
2020-12-24 13:37:54 +1100

    PR feedback and fixed weird title

--- src/openapi3/handleJson.ts
+++ src/openapi3/handleJson.ts
@@ -3,7 +3,7 @@ import { OpenAPIV3 } from "openapi-types";
 import { Field } from "../Field";
 import { Resource } from "../Resource";
 import { getResources } from "../utils/getResources";
-import jsonRefs, { ResolvedRefsResults } from "json-refs";
+import jsonRefs from "json-refs";
 
 export const removeTrailingSlash = (url: string): string => {
   if (url.endsWith("/")) {
@@ -13,12 +13,12 @@ export const removeTrailingSlash = (url: string): string => {
 };
 
 /* Assumptions:
-  RESTful APIs typically have two paths per resources -  a `/noun` path and a 
+  RESTful APIs typically have two paths per resources:  a `/noun` path and a 
   `/noun/{id}` path. `getResources` strips out the latter, allowing us to focus
   on the former.
 
-  In OpenAPI3, the `/noun` path will typically have a `get` action, that 
-  probably accepts parameters and would respond with a array of objects, 
+  In OpenAPI 3, the `/noun` path will typically have a `get` action, that 
+  probably accepts parameters and would respond with an array of objects, 
   described in the `items` field.
 */
 
@@ -26,8 +26,8 @@ export default async function (
   response: OpenAPIV3.Document,
   entrypointUrl: string
 ): Promise<Resource[]> {
-  const results: ResolvedRefsResults = await jsonRefs.resolveRefs(response);
-  const document: OpenAPIV3.Document = results.resolved as OpenAPIV3.Document;
+  const results = await jsonRefs.resolveRefs(response);
+  const document = results.resolved as OpenAPIV3.Document;
 
   const paths = getResources(document.paths);
 
@@ -45,8 +45,9 @@ export default async function (
     const url = removeTrailingSlash(serverUrl) + item;
 
     const method = document.paths[item].get as OpenAPIV3.OperationObject;
-
     if (!method) return;
+
+    const title = get(method, ["tags", "0"], name) as string;
 
     const schema = get(
       method,
@@ -81,7 +82,7 @@ export default async function (
     resources.push(
       new Resource(name, url, {
         id: null,
-        title: "title",
+        title: title,
         fields,
         readableFields: fields,
         writableFields: fields,
