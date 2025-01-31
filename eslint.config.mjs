d638a9a099ebceb197e9653465111908962daadc
Kévin Dunglas <kevin@dunglas.fr>
2025-01-31 17:36:40 +0100

    ...

---
+++ eslint.config.mjs
@@ -0,0 +1,70 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+    "plugin:prettier/recommended",
+)), {
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.commonjs,
+            ...globals.jest,
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 7,
+        sourceType: "module",
+
+        parserOptions: {
+            project: ["./tsconfig.eslint.json"],
+        },
+    },
+
+    settings: {
+        "import/resolver": {
+            typescript: true,
+            node: true,
+        },
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+        "@typescript-eslint/consistent-type-imports": "error",
+
+        "@typescript-eslint/no-empty-interface": ["error", {
+            allowSingleExtends: true,
+        }],
+
+        "@typescript-eslint/no-use-before-define": ["error", {
+            functions: false,
+        }],
+
+        "@typescript-eslint/no-explicit-any": [0],
+        "import/no-named-as-default-member": [0],
+    },
+}];
