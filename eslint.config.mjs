bfaf476f6547dc06115676c6ab3bd5e56255d617
Kévin Dunglas <kevin@dunglas.fr>
2025-01-31 18:54:31 +0100

    cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,81 +1,13 @@
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default [
-  ...fixupConfigRules(
-    compat.extends(
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      "plugin:import/recommended",
-      "plugin:import/typescript",
-      "prettier",
-      "plugin:prettier/recommended",
-    ),
-  ),
-  {
-    plugins: {
-      "@typescript-eslint": fixupPluginRules(typescriptEslint),
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.commonjs,
-        ...globals.jest,
-        ...globals.node,
-      },
-
-      parser: tsParser,
-      ecmaVersion: 7,
-      sourceType: "module",
-
-      parserOptions: {
-        project: ["./tsconfig.eslint.json"],
-      },
-    },
-
-    settings: {
-      "import/resolver": {
-        typescript: true,
-        node: true,
-      },
-    },
-
-    rules: {
-      "prettier/prettier": "error",
-      "@typescript-eslint/consistent-type-imports": "error",
-
-      "@typescript-eslint/no-empty-interface": [
-        "error",
-        {
-          allowSingleExtends: true,
-        },
-      ],
-
-      "@typescript-eslint/no-use-before-define": [
-        "error",
-        {
-          functions: false,
-        },
-      ],
-
-      "@typescript-eslint/no-explicit-any": [0],
-      "import/no-named-as-default-member": [0],
-    },
-  },
-];
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+);
