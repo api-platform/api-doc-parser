1df446a15f14afd64903cdb50d04092194d5f6b1
J3m5 <5523410+J3m5@users.noreply.github.com>
2025-06-09 22:50:29 +0200

    fix(oxlint): unicorn/prefer-spread
    https://oxc.rs/docs/guide/usage/linter/rules/unicorn/prefer-spread.html

--- src/utils/getResources.ts
+++ src/utils/getResources.ts
@@ -3,11 +3,11 @@ import type { OpenAPIV2, OpenAPIV3 } from "openapi-types";
 export default function getResources(
   paths: OpenAPIV2.PathsObject | OpenAPIV3.PathsObject,
 ): string[] {
-  return Array.from(
-    new Set(
+  return [
+    ...new Set(
       Object.keys(paths).filter((path) => {
         return new RegExp("^[^{}]+/{[^{}]+}/?$").test(path);
       }),
     ),
-  );
+  ];
 }
