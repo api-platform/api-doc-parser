9d32422fb72198eb5d6f21f8d5c4dcce20c8303f
J3m5 <5523410+J3m5@users.noreply.github.com>
2025-07-14 12:55:18 +0200

    refactor(utils): rename getResource to getResourcePaths, use named export
    Signed-off-by: J3m5 <5523410+J3m5@users.noreply.github.com>

--- src/core/utils/getResourcePaths.ts
+++ src/core/utils/getResourcePaths.ts
@@ -1,6 +1,6 @@
 import type { OpenAPIV2, OpenAPIV3 } from "openapi-types";
 
-export default function getResources(
+export function getResourcePaths(
   paths: OpenAPIV2.PathsObject | OpenAPIV3.PathsObject,
 ): string[] {
   return [
